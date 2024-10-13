c8686288a74df02b8e220399e023cd14376009c8
SatyamDevGenie <satyamsawant54@gmail.com>
2024-10-13 22:05:43 +0530

    expiry date extended

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "90d" });
 };
 export default generateToken;
